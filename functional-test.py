9247044b330393512ae4b843a8eb98d10e56faf2
Minjae Shin <neplyudof@gmail.com>
2016-06-16 20:58:40 +0900

    Init Commit

---
+++ functional-test.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import unittest
+
+from selenium import webdriver
+
+
+class FirstUserTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_visit_main_page(self):
+        # memsys를 처음 사용하는 사용자가 페이지를 방문한다
+        self.browser.get('http://localhost:8080')
+
+        # 웹 페이지 타이틀과 헤더에 'Memory Analasis'를 표시하고 있다
+        # self.assertEqual('Memory Analasis', self.browser.title)
+        self.assertIn(u'Django', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
