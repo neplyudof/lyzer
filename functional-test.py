a47a1e239508f321d77524c88f34a00e2daa6966
Minjae Shin <neplyudof@gmail.com>
2016-06-16 21:21:39 +0900

    최소 HTML을 반환하는 메인 페이지 뷰 추가

--- functional-test.py
+++ functional-test.py
@@ -16,9 +16,8 @@ class FirstUserTest(unittest.TestCase):
         # memsys를 처음 사용하는 사용자가 페이지를 방문한다
         self.browser.get('http://localhost:8080')
 
-        # 웹 페이지 타이틀과 헤더에 'Memory Analasis'를 표시하고 있다
-        # self.assertEqual('Memory Analasis', self.browser.title)
-        self.assertIn(u'Django', self.browser.title)
+        # 웹 페이지 타이틀과 헤더에 'Lyzer'를 표시하고 있다
+        self.assertIn(u'Lyzer', self.browser.title)
 
 
 if __name__ == '__main__':
--- memsis/tests.py
+++ memsis/tests.py
@@ -1,8 +1,29 @@
+from unittest import skip
+
+from django.http import HttpRequest
 from django.test import TestCase
 
-
 # Create your tests here.
+from django.urls import resolve
 
+from memsis import views
+
+@skip
 class SmokeTest(TestCase):
     def test_bad_math(self):
         self.assertEqual(1 + 1, 3)
+
+
+class MainPageTest(TestCase):
+    def test_root_url_resolve(self):
+        root_url = resolve('/')
+
+        self.assertEqual(root_url.func, views.index)
+
+    def test_returns_correct_main_page(self):
+        request = HttpRequest()
+        response = views.index(request)
+
+        self.assertTrue(response.content.startswith('<html>'))
+        self.assertIn(b'<title>Lyzer</title>', response.content)
+        self.assertTrue(response.content.endswith('</html>'))
